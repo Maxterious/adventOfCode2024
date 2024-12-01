a57eec4baa2684ccb68216fcec2acbfbfa27fa45
Maxterious <teichmann.max@gmx.de>
2024-12-01 21:07:17 +0100

    day one part one

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -1,5 +1,9 @@
 package day1
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val input = File("src/main/kotlin/day1/input.txt").readLines()
+    val result = Part1(input).calculateDistanceSum()
+    println("The total distance is: $result")
 }
--- src/main/kotlin/day1/Part1.kt
+++ src/main/kotlin/day1/Part1.kt
@@ -1,4 +1,34 @@
-package org.example.day1
+package day1
 
-class Part1 {
+class Part1(private val input: List<String>) {
+    private val listOne = mutableListOf<Int>()
+    private val listTwo = mutableListOf<Int>()
+
+    fun calculateDistanceSum(): Int {
+        input.forEach {
+            splitStringIntoNumbers(it)
+        }
+
+        listOne.sort()
+        listTwo.sort()
+        var totalDistance = 0
+        listOne.forEachIndexed { index, _ ->
+            totalDistance += calculateNumberDistance(listOne[index], listTwo[index])
+        }
+        return totalDistance
+    }
+
+    private fun splitStringIntoNumbers(inputLine: String) {
+        listOne.add(inputLine.substringBefore(" ").toInt())
+        listTwo.add(inputLine.substringAfter(" ").trim().toInt())
+    }
+
+    private fun calculateNumberDistance(numberOne: Int, numberTwo: Int): Int {
+        //bigger number minus smaller number
+        return if (numberOne > numberTwo) {
+            numberOne - numberTwo
+        } else {
+            numberTwo - numberOne
+        }
+    }
 }
